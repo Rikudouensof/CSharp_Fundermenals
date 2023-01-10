0996e37a439511ac7ce137a50607d87c1a308ddc
Rikudou En Sof <rikudouensof@outlook.com>
2023-01-10 10:49:58 +0100

    Vale Data Types

--- CSharp_Fundermenals.Console/Program.cs
+++ CSharp_Fundermenals.Console/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharp_Fundermenals.Basics.DataTpes;
+
+
+ValueDataTypes.Get_Signed_Data_Type_Values();
+ValueDataTypes.Get_UnSigned_Data_Type_Values();
+ValueDataTypes.Get_FloatingPoint_DataTpe();
+ValueDataTypes.Get_Decimal_DataTpe();
+ValueDataTypes.Get_Char_DataType();
+ValueDataTypes.Get_Bool_DataTpe();
+
+Console.ReadKey();
