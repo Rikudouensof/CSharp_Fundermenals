dc07abf5f83b500ba80173e19fdefbaee7689487
Rikudou En Sof <rikudouensof@outlook.com>
2023-01-11 19:35:11 +0100

    Data Types Converion Start

--- CSharp_Fundermenals.Console/Program.cs
+++ CSharp_Fundermenals.Console/Program.cs
@@ -1,14 +1,6 @@
 ﻿using CSharp_Fundermenals.Basics.DataTpes;
 
+//DataType Convertions
 
-ValueDataTypes.Get_Signed_Data_Type_Values();
-ValueDataTypes.Get_UnSigned_Data_Type_Values();
-ValueDataTypes.Get_FloatingPoint_DataTpe();
-ValueDataTypes.Get_Decimal_DataTpe();
-ValueDataTypes.Get_Char_DataType();
-ValueDataTypes.Get_Bool_DataTpe();
-RefrenceDataTypes.Get_String_DataType();
-RefrenceDataTypes.Get_Object_DataType();
-PointerDataType.Get_Pointer_DataType();
 
 Console.ReadKey();
