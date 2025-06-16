55019242831ac9c8a26a2093847fa21b8246d324
Solomon-Uke-DotnetBackend-fcmb <163281200+Solomon-Uke-DotnetBackend-fcmb@users.noreply.github.com>
2025-06-16 19:55:58 +0100

    Save

---
+++ CSharp_Fundermenals.Basics/Collections/ArrayLesson.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Fundermenals.Basics.Collections
+{
+    public class ArrayLesson
+    {
+        //Declaration with array index. Can be instanciated or not and it will work
+        string[] cars = new string[3] { "Volvo", "BMV", "Cargo" };
+
+
+        //Declaration without defining the size of index, it is set by the number of values
+        string[] cars2 = { "Benz", "Lexus" };
+
+        string[] planets =
+        {
+            "Mercury", "Venus","Eearth","Mars","Jupiter","Sarturn","Uranus","Neptune"
+        };
+
+       
+
+
+        public void GetArrayIteration()
+        {
+            foreach (string planet in planets)
+            {
+
+                Console.WriteLine(planet);
+            }
+        }
+
+
+        //Remeber Arrays use O index
+        public void GetArrayValueByIndex(int index)
+        {
+            Console.WriteLine($"in the planet array, the planet: {planets[index]} is for index: {index}");
+        }
+
+        public void UpdateArrayValue(int index, string newValue)
+        {
+            planets[index] = newValue;
+
+            Console.WriteLine("New Array iteration");
+            GetArrayIteration();
+        }
+
+        public void OtherArrayAbilities()
+        {
+            //Reverses the value of the array content, replaces value of the same array name
+            Console.WriteLine("Reverse Array");
+            Array.Reverse(planets);
+            GetArrayIteration();
+            Console.WriteLine();
+
+            //Sorts the order of Array Item, replaces value of the same array name
+            Console.WriteLine("Sort Array");
+            Array.Sort(planets);
+            GetArrayIteration();
+            Console.WriteLine();
+        }
+
+    }
+
+}
--- CSharp_Fundermenals.Console/Program.cs
+++ CSharp_Fundermenals.Console/Program.cs
@@ -1,14 +1,21 @@
-﻿using CSharp_Fundermenals.Basics.DataTpes;
+﻿using CSharp_Fundermenals.Basics.Collections;
+using CSharp_Fundermenals.Basics.DataTpes;
 
 
-ValueDataTypes.Get_Signed_Data_Type_Values();
-ValueDataTypes.Get_UnSigned_Data_Type_Values();
-ValueDataTypes.Get_FloatingPoint_DataTpe();
-ValueDataTypes.Get_Decimal_DataTpe();
-ValueDataTypes.Get_Char_DataType();
-ValueDataTypes.Get_Bool_DataTpe();
-RefrenceDataTypes.Get_String_DataType();
-RefrenceDataTypes.Get_Object_DataType();
-PointerDataType.Get_Pointer_DataType();
+//ValueDataTypes.Get_Signed_Data_Type_Values();
+//ValueDataTypes.Get_UnSigned_Data_Type_Values();
+//ValueDataTypes.Get_FloatingPoint_DataTpe();
+//ValueDataTypes.Get_Decimal_DataTpe();
+//ValueDataTypes.Get_Char_DataType();
+//ValueDataTypes.Get_Bool_DataTpe();
+//RefrenceDataTypes.Get_String_DataType();
+//RefrenceDataTypes.Get_Object_DataType();
+//PointerDataType.Get_Pointer_DataType();
+
+ArrayLesson arrayobj = new ArrayLesson();
+//arrayobj.GetArrayIteration();
+//arrayobj.GetArrayValueByIndex(3);
+//arrayobj.UpdateArrayValue(3,"The Red planet");
+arrayobj.OtherArrayAbilities();
 
 Console.ReadKey();
